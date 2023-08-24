069f0fc76d1f11809922471d45c5b5d22e402ec7
Abilash-devops <abilashdevopsaws@gmail.com>
2023-08-24 15:49:53 +0530

    commited

---
+++ catalogue.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+D=$(date +%F:%H:%M:%S)
+SCRIPT_NAME=$0
+LOG_PATH=/home/centos/roboshell/logs
+LOGFILE=$LOG_PATH/$0-$D-log
+u=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+if [ $u -ne 0 ]
+then
+echo " Please run the script with root or superuser previliges "
+exit 1
+fi
+validate(){
+    if [ $? -ne 0 ]
+    then 
+        echo -e " $2 is $R FAILURE $N"
+        exit 1
+    else
+        echo -e " $2 is $G SUCCESS $N"
+    fi
+}
