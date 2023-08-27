bdf7bd25fd7443ef7c7c3662aecdd75bf199d3ec
Abilash-devops <abilashdevopsaws@gmail.com>
2023-08-27 20:54:56 +0530

    Commited

--- abiec2.sh
+++ abiec2.sh
@@ -14,6 +14,16 @@ do
     else
         INSTANCE_TYPE=t2.micro
     fi
+    instance_ids=$(aws ec2 describe-instances --filters Name=tag:Name,Values="'$i'" | jq -r '.Reservations[].Instances[].InstanceId')
+        for instance in $instance_ids
+        do
+            running=$(aws ec2 describe-instances --instance-ids $instance | jq -r '.Reservations[].Instances[].State.Name')
+            if [ $running == running ]
+            then
+                echo " I cant launch new instance because your previous script insatnces are runnng. Please do terminate previous instance and launch the script again"
+                exit 1
+            fi
+        done    
     echo "Creating $i instance"
     b=$(aws ec2 run-instances --image-id $image_id --instance-type $INSTANCE_TYPE --security-group-ids $sg_id --subnet-id $subnet_id --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
     echo " For $i instance private IP adress is :$b"
